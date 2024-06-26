a6fee80f9e348635fae8fe01f55a930402001a5b
Nafis-Hasnat369 <nafis7823@gmail.com>
2024-06-27 02:09:24 +1000

    super user created

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from about.models import Teacher
 
 # Register your models here.
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'teacher_id', 'teacher_name', 'teacher_email')
+
+
+admin.site.register(Teacher, TeacherAdmin)
