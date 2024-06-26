44ef8c6668e251d2a421437e11c8ca7fcd582965
Nafis-Hasnat369 <nafis7823@gmail.com>
2024-06-26 23:32:51 +1000

    about page dynamic urls fixed

--- about/models.py
+++ about/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Teacher(models.Model):
+    # Define your attribute
+    teacher_id = models.IntegerField()
+    teacher_name = models.CharField(max_length=40)
+    teacher_email = models.EmailField(max_length=30)
+    
